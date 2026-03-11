fe73ef13301e643fb306cedcbe2fb78c10a269bc
jamesbarnard523 <jamesbarnard8944@gmail.com>
2026-03-11 13:50:36 +0000

    update

--- problemsets 9-2.py
+++ problemsets 9-2.py
@@ -1,21 +1,24 @@
 import math
 
-class Circle:
-    def __init__(self, radius):
+class circle:
+    def __init__(self,radius):
         self.radius = radius
+        return
 
-    def update_radius(self, new_radius):
-        self.radius = new_radius
+    def updateRadius(self,updateradius):
+        self.updateradius = self.radius
+        return
+        
+    def getcircumference(self):
+        return  2 * math.pi * (self.radius**2)
+        
     
-    def get_circumference(self):
-        return 2 * math.pi * self.radius
+    def getarea(self):
+        return  math.pi * (self.radius**2)
+        
     
-    def get_area(self):
-        return math.pi * self.radius ** 2
+shape = circle(5)
 
-c = Circle(5)
+print(f'area of circle with radius : {shape.radius} equals:\n {shape.getarea()} ')
 
-print(f"Radius:        {c.radius}")
-
-print(f"circuference: {c.get_circumference():}")
-print(f"area:          {c.get_area():}")
+print (f'circumference of circle with radius {shape.radius} equals\n {shape.getcircumference()}')
--- problemsets9-4.py
+++ problemsets9-4.py
@@ -1,4 +1,31 @@
 #Write a program that prompts the user to enter the location of two coordinates, and prints to the screen
-#the equation of the straight line, y= mx+ cthat connects them. A bespoke class should be designed
+#the equation of the straight line, y= mx+ c that connects them. A bespoke class should be designed
 #to represent a coordinate (this should hold two values), and another class to represent a straight line
 #(this should contain two coordinates and any required functionality).
+
+class coord:
+    def __init__(self, x1 , y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+
+        self.x2 = x2
+        self.y2 = y2
+
+    def gradientline(self):
+        m = (self.y2-self.y1) / (self.x2-self.x1)
+        c = self.y1 - (self.x1 * m)
+        self.gradient = m*x2 + c
+        return self.gradient 
+    
+    def equation(self):
+        m = self.gradientline()
+        c = self.y1 - (m * self.x1)
+        return f'y = {m}x + {c}'
+
+x1,y1,x2,y2 = map(int,input('enter coord values x1 y1 x2 y2 (space-sep): ').split())
+
+coordinate = coord(x1,y1,x2,y2)
+
+print(f'Gradient: {coordinate.gradientline()}')
+print(f'Equation: {coordinate.equation()}')
+        
